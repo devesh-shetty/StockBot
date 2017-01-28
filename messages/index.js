8ac98f6557833bc0b6751eecfc41f095785ab15c
Devesh Shetty <devesh.shetty007@gmail.com>
2017-01-29 01:20:58 +0530

    Modified selectors

--- messages/index.js
+++ messages/index.js
@@ -54,8 +54,13 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
             var title, release, rating;
             var json = { price : "", name : "", rating : ""};
 
-            json.name = $('h1').attr('data-reactid','250').text();
-            json.price = $('span').attr('data-reactid','279').text();
+            // json.name = $('h1').attr('data-reactid','250').text();
+            // json.price = $('span').attr('data-reactid','279').text();
+
+            json.name = $('h1[data-reactid="250"]').val();
+            json.price = $('span[data-reactid="279"]').val();
+            console.log(json);
+
             session.send('Hi! This is the stock intent handler. You said: \'%s (%s)\'.', json.name,
          json.price);
         }
